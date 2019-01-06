dfab86e422ef87545c3ebeb226082e92d20b034f
kallejre <kallejre@yahoo.com>
2019-01-06 13:34:11 +0200

    Konsolideerimine
    Kõik teised programmid olid d_ani_class.py-s esindatud ja kustutasin.
    discord_animatsioon_class.py -> discord_class.py

--- discord_class.py
+++ discord_class.py
@@ -81,6 +81,10 @@ class Stats:
         self.users = dict()
         self.lyhi = dict()
         self.times = list()
+        self.times2 = dict()
+        # self.ajaformaat = '%a %d %b %Y %H:00'  # 'Thu 15 Nov 2018 14:00'
+        self.ajaformaat = '%a %d %b %Y'          # Kasutusel koos times2-ga
+        # max(sts.times2,key=lambda x:datetime.datetime.strptime(x,sts.ajaformaat))
         for x1 in range(len(self.archive['meta']['userindex'])):
             self.users[x1] = {'n': self.archive['meta']['users'][self.archive['meta']['userindex'][x1]]['name'],
                               'count': dict(), 'lens': dict(), 'times': dict(), 'next': dict(),
@@ -109,7 +113,7 @@ class Stats:
                             "pr14": {"PR", "Kokku"}, "pr15": {"PR", "Kokku"}, "random": {"Yldine", "Kokku"},
                             "wat": {"PR", "Kokku"},
                             "stat": {"Yldine", "Kokku"}, "syvapy-general": {"Yldine", "Kokku"},
-                            "videod": {"Yldine", "Kokku"},
+                            "videod": {"Yldine", "Kokku"}, "eksam": {"Yldine", "Kokku"},
                             "xp01": {"Syva", "Kokku", "XP"}, "xp02": {"Syva", "Kokku", "XP"},
                             "xp03": {"Syva", "Kokku", "XP"},
                             "xp04": {"Syva", "Kokku", "XP"}, "xp05": {"Syva", "Kokku", "XP"},
@@ -140,23 +144,22 @@ class Stats:
         Kunagi võiks teha mingi seadistamisvõimaluse.
         """
         f = open('disc_sõnapilveks.txt', 'w', encoding='utf8')
-        for c in self.archive['data']:  # c = kanali id
+        for c in sorted(self.archive['data']):  # c = kanali id
             cur_name = self.archive['meta']['channels'][c]['name']  # Praeguse kanali nimi
             cur_name = cur_name.split('_')[0]
             prev_msg = -1
             print(cur_name[:1] + cur_name[-2:], end=' ')
-            for m in sorted(self.archive['data'][c]):               # m = sõnumi ID
+            for m in sorted(self.archive['data'][c]):  # m = sõnumi ID
                 message = self.archive['data'][c][m]
-                if len(message['m'].strip().split()) < 2:           # Ühesõnaliste sõnumitega opereerimine
+                if len(message['m'].strip().split()) < 2:  # Ühesõnaliste sõnumitega opereerimine
                     if message['u'] not in self.lyhi:
-                        self.lyhi[message['u']] = 0                 # Loendab lühikesi sõnumeid
+                        self.lyhi[message['u']] = 0  # Loendab lühikesi sõnumeid
                     self.lyhi[message['u']] += 1
                     # continue                                      # Lühikese sõnumi saab vahele jätta
-                print(message['m'].lower(), file=f)                 # Kopeeri sõnum faili
+                print(message['m'].lower(), file=f)  # Kopeeri sõnum faili
                 time = datetime.datetime.fromtimestamp(message['t'] // 1000)  # 1 sekundi täpsusega
                 wk = time.weekday()
                 hr = time.hour
-
                 self.times.append(time)
                 uid = message['u']
                 # REGEX start
@@ -182,23 +185,32 @@ class Stats:
                     self.users[uid]['next'][prev_msg] += 1
                     self.users[prev_msg]['prev'][uid] += 1
                 # REGEX läbi
-
+                # self.times2 töötlemine
+                time_str = time.strftime(self.ajaformaat)
+                if time_str not in self.times2:
+                    self.times2[time_str] = dict()
+                if cur_name not in self.times2[time_str]:
+                    self.times2[time_str][cur_name] = dict()
+                if uid not in self.times2[time_str][cur_name]:
+                    self.times2[time_str][cur_name][uid] = 0
+                self.times2[time_str][cur_name][uid] += 1
+                # self.times2 töötlemine läbi
                 for sub in [cur_name] + list(self.kategooriad[cur_name]):
-                    if sub not in self.users[uid]['count']:         # Kui see sõnum on kasutaja
-                        self.users[uid]['count'][sub] = 0           # esimene sõnum antud kanalis
-                        self.users[uid]['lens'][sub] = 0            # init loendurid
-                        self.users[uid]['times'][sub] = [0] * 168   # Iga tunni jaoks
+                    if sub not in self.users[uid]['count']:  # Kui see sõnum on kasutaja
+                        self.users[uid]['count'][sub] = 0  # esimene sõnum antud kanalis
+                        self.users[uid]['lens'][sub] = 0  # init loendurid
+                        self.users[uid]['times'][sub] = [0] * 168  # Iga tunni jaoks
                     self.users[uid]['count'][sub] += 1
                     self.users[uid]['lens'][sub] += len(message['m'])
                     self.users[uid]['times'][sub][24 * wk + hr] += 1
 
                 uid2 = uid
-                uid = -1                                            # Kõigi kasutajate ühisstatistika
+                uid = -1  # Kõigi kasutajate ühisstatistika
                 for sub in [cur_name] + list(self.kategooriad[cur_name]):
-                    if sub not in self.users[uid]['count']:         # Kui see sõnum on kasutaja
-                        self.users[uid]['count'][sub] = 0           # esimene sõnum antud kanalis
-                        self.users[uid]['lens'][sub] = 0            # init loendurid
-                        self.users[uid]['times'][sub] = [0] * 168   # Iga tunni jaoks
+                    if sub not in self.users[uid]['count']:  # Kui see sõnum on kasutaja
+                        self.users[uid]['count'][sub] = 0  # esimene sõnum antud kanalis
+                        self.users[uid]['lens'][sub] = 0  # init loendurid
+                        self.users[uid]['times'][sub] = [0] * 168  # Iga tunni jaoks
                     self.users[uid]['count'][sub] += 1
                     self.users[uid]['lens'][sub] += len(message['m'])
                     self.users[uid]['times'][sub][24 * wk + hr] += 1
@@ -220,10 +232,9 @@ class Stats:
                     self.users[uid]['next'][prev_msg] += 1
                     self.users[uid]['prev'][uid2] += 1
                 prev_msg = uid2
-
+        print()
         self.times = list(sorted(self.times))
         f.close()
-        print()
 
     def arhiiv(self):
         """
@@ -289,7 +300,7 @@ class Stats:
         ajad = [self.header2]
         for x1 in list(self.users):
             total = []
-            for c in sorted(self.users[x1]['times']):               # Iga kanaliga
+            for c in sorted(self.users[x1]['times']):  # Iga kanaliga
                 total.append(self.users[x1]['times'][c])
                 ajad.append('\t'.join([self.users[x1]['n'], c] + list(map(str, total[-1]))))
             total = list(map(sum, list(zip(*total))))
@@ -417,7 +428,7 @@ class Stats:
     def stat_msg(self):
         """Sõnumite statisika, kui palju on X->Y sõnumeid."""
         with open('d_out_msg.txt', 'w', encoding='utf-8') as f:
-            print('X Y X->Y'.split(), file=f, sep='\t')
+            print('\t'.join('X Y X->Y').split(), file=f, sep='\t')
             for uid in self.users:
                 for nxt in self.users[uid]['next']:
                     count = self.users[uid]['next'][nxt]
@@ -459,13 +470,40 @@ class Stats:
                             if count > 1:
                                 l = list(sorted([self.users[uid]['n'], self.users[nxt]['n']]))
                                 print(*l + [valja, sisse, str(count).replace('.', ',')], file=f, sep='\t')
+    def stat_last_24():
+        """Kasutajate aktiivsus viimased 24 tundi või päeva."""
+        e=sorted(sts.times2,key=lambda x:datetime.datetime.strptime(x,sts.ajaformaat))[-24:]
+        q=set()
+        usrs=set()
+        for i in e:
+            q=q.union(sts.times2[i])
+            for x in sts.times2[i]:
+                usrs=usrs.union(set(sts.times2[i][x]))
+        usrs=list(sorted(usrs))
+        us2=list(map(lambda x: sts.users[x]['n'],usrs))
+        q=list(sorted(q))
+        with open('d_out_last24.txt', 'w', encoding='utf-8') as f:
+            print('\t'.join(['Kuupäev','Kanal']+us2), file=f)
+            for i in e:
+                for kanal in q:
+                    out=[]
+                    if kanal not in sts.times2[i]:
+                        continue
+                    out.append(i)
+                    out.append(kanal)
+                    for use in usrs:
+                        if use in sts.times2[i][kanal]:
+                            out.append(sts.times2[i][kanal][use])
+                        else:
+                            out.append(0)
+                    print('\t'.join(list(map(str,out))), file=f)
 
     def graafik(self):
         """Pygame joonistamine. Väga KATKI!"""
         import pygame
         colors, c = dict(), 0
         for i1 in self.channels:
-            colors[i1] = tuple(map(lambda x: 255 * x, colorsys.hsv_to_rgb(0.618033988749895 * c, 1, 1)))
+            colors[i1] = tuple(map(lambda x: round(255 * x), colorsys.hsv_to_rgb(0.618033988749895 * c, 1, 1)))
             c += 1
         pygame.init()
         lava = pygame.display.set_mode((min(len(self.times), 10000), len(self.users)), 0, 32)
@@ -483,10 +521,228 @@ class Stats:
         for i1 in sorted(colors):
             print(i1, colors[i1])
         pygame.quit()
+    def times2_cleanup(self,n=25):
+        ###  ----   Times2/times3 Eri
+        # Lugeda kokku enim postituste TOP_N (25) ja ülejäänute statistika liita.
+        self.top_n=list(filter(lambda x:x>=0,sorted(self.users,key=lambda x:self.users[x]['count']['Kokku'],reverse=True)))[:n]
+        print(self.top_n)
+        for date in self.times2:
+            for kanal in self.times2[date]:
+                counter=0
+                for uid in list(self.times2[date][kanal]):
+                    if uid not in self.top_n:
+                        counter+=self.times2[date][kanal][uid]
+                        del self.times2[date][kanal][uid]
+                self.times2[date][kanal][-1]=counter
+        ###  ----   Times2/times3 Eri läbi
 
+
+class Animate:
+    ### Kogu soust tuleb nüüd ümber kirjutada
+    def __init__(self, sts):
+        self.s = 20  # X-telje märgete kirjasuurus                                  # Default: 50
+        self.ajatempel = 25  # Ajatempli kõrgus ülal vasakus nurgas                 # Default: 50
+        self.legend_size = 30  # Legendi kirjakõrgus                                # Default: 30
+        self.name_buffer = self.s * 2 + 5  # Eeldatav nimede ruum koos 5px varuga
+        self.colors = dict()
+        self.sts = sts
+        self.width = (len(sts.channels) + 1) * self.s
+        self.graafiku_osa = 250
+        self.font_name = 'agencyFB'
+        pygame.font.init()
+        # self.sts.top_n
+        # self.sts.users
+        sizes = self.draw_user_legend(simulate=True)
+        ls = len(sizes)
+        sizes.sort()
+        x=0.9
+        self.x2 = sizes[round(ls * x)]
+        self.y2 = self.legend_size + 3
+        x_columns = self.width // self.x2
+        y_lines = (len(self.sts.top_n) - 1) // x_columns+1
+        self.y_hei = y_lines * self.y2
+        self.day0 = datetime.datetime.strptime(min(sts.times2,key=lambda x:datetime.datetime.strptime(x,sts.ajaformaat)),sts.ajaformaat)
+        self.day9 = datetime.datetime.strptime(max(sts.times2,key=lambda x:datetime.datetime.strptime(x,sts.ajaformaat)),sts.ajaformaat)
+        c = 0
+        for i in list(filter(lambda x: x >= 0, sts.top_n)):
+            self.colors[i] = tuple(map(lambda x: min([round(255 * x), 255]),
+                            colorsys.hsv_to_rgb(0.618033988749895 * c, 1 - i // 5 * 0.01, 1 - i // 3 * 0.005)))
+            c += 1
+        # """
+        self.colors[-1]=(50,50,50)  # Ülejäänud
+        self.colors[0]=(200,200,200)  # Rauno?
+        self.colors[1]=(255,200,200)  # Kadri
+        self.colors[2]=(200,255,200)  # Ago
+        self.colors[3]=(200,200,255)  # Test9
+        self.colors[4]=(255,255,200)  # Ergo
+        self.hei = self.graafiku_osa + self.y_hei + self.name_buffer + self.ajatempel
+        maksimumid = set()
+        for time in sts.times2:
+            for kanal in sts.times2[time]:
+                s = 0
+                for uid in sts.times2[time][kanal]:
+                    s += sts.times2[time][kanal][uid]
+                maksimumid.add(s)
+        self.vahemiku_max = max(maksimumid)
+
+    def draw_timestamp(self, stamp, pos=(5, 0), color=(200, 000, 000), font='agencyFB'):
+        stamp = str(stamp)
+        font = pygame.font.SysFont(self.font_name, int(self.ajatempel * 0.8333333))
+        text = font.render(stamp, True, color)
+        self.lava.blit(text, pos)
+
+    def draw_x_axis(self, channels):
+        # X-telje joonistamine
+        y_t = self.hei - self.name_buffer - self.y_hei
+        color = (200, 000, 000)
+        
+        font = pygame.font.SysFont(self.font_name, int(self.s * 0.8333333))
+        for i in range(len(self.sts.channels)):
+            x_t = (i * self.s)
+            txt = str(channels[i])
+            text = font.render(txt, True, color)
+            overflow = pygame.surface.Surface((self.s * 2, int(self.s * 1)))
+            overflow.blit(text, (0, 0))
+            overflow = pygame.transform.rotate(overflow, 90)
+            self.lava.blit(overflow, (x_t, y_t))
+
+    def draw_user_legend(self, simulate=False):
+
+        # Legendi joonistamine:
+        if not simulate:
+            y = self.hei - self.y_hei
+            x = 10
+        size = self.legend_size
+        font = pygame.font.SysFont(self.font_name, int(size * 0.8333333))
+        txt_color = (200, 000, 000)
+        out = []
+        for i in list(filter(lambda x: x >= 0, self.sts.top_n))+[-1]:
+
+            txt = self.sts.users[i]['n']
+            text = font.render(txt, True, txt_color)
+            overflow = pygame.surface.Surface((text.get_size()[0] + int(size * 0.8), int(size)))
+            if not simulate:
+                overflow.blit(text, (int(size * 0.8), 0))
+                pygame.draw.rect(overflow, self.colors[i],
+                                 (int(size * 0.1), int(size * 0.2), int(size * 0.6), int(size * 0.6)))
+                self.lava.blit(overflow, (x, y))
+
+                y += self.y2
+                if y + self.y2 > self.hei:
+                    y = self.hei - self.y_hei
+                    x += self.x2
+
+            out.append(overflow.get_size()[0])
+        return out
+    def draw_column(self,kanal,data):
+        """Koosta standartse laiuse ja kõrgusega tulp, ära joonista."""
+        # Ei tulnud hästi välja. Järgmine idee: TOP25 ja ülejäänud
+        w,h=self.s,self.graafiku_osa+self.ajatempel*1
+        column=pygame.surface.Surface((w,h))
+        column_nr = ani.sts.channels.index(kanal)
+        s = 0
+        for uid in sorted(data[kanal],reverse=True):
+            # Joonista kasutaja rect
+            y_h=round(log(s+1)/log(self.vahemiku_max+1)*self.graafiku_osa)
+            y_h2=round(log(s+data[kanal][uid]+1)/log(self.vahemiku_max+1)*self.graafiku_osa)
+            y_h=round((s)/(self.vahemiku_max)*self.graafiku_osa)  # Loobusin logaritmidest
+            y_h2=round((s+data[kanal][uid])/(self.vahemiku_max)*self.graafiku_osa)
+            pygame.draw.rect(column,self.colors[uid], (0, h-y_h, w,h-y_h2))
+            
+            s += data[kanal][uid]
+        return column
+        #y_h=round(log(s+1)/log(self.vahemiku_max+1)*self.graafiku_osa)
+        #pygame.draw.rect(self.lava,[220]*3,(self.s*column_nr,self.graafiku_osa+self.ajatempel-y_h,self.s,y_h))
+    def draw_columns(self,data=None):
+        if not data:
+            return
+        # Tulpade joonistamine
+        # self.vahemiku_max
+        
+        self.ajatempel  # Ajatempli kõrgus ülal vasakus nurgas
+        self.graafiku_osa
+        for kanal in data:
+            column_nr = self.sts.channels.index(kanal)
+            out=self.draw_column(kanal,data)
+            self.lava.blit(out,(self.s*column_nr,0))
+            """
+            column_nr = ani.sts.channels.index(kanal)
+            s = 0
+            for uid in data[kanal]:
+                s += data[kanal][uid]
+            y_h=round(log(s+1)/log(self.vahemiku_max+1)*self.graafiku_osa)
+            pygame.draw.rect(self.lava,[220]*3,(self.s*column_nr,self.graafiku_osa+self.ajatempel-y_h,self.s,y_h))
+            """
+    def draw(self,stamp):
+        """Ühe kaadri joonistamine."""
+        self.lava.fill([0,0,0])
+        self.draw_x_axis(sts.channels)        # X-telje joonistamine
+        self.draw_user_legend()        # Legendi joonistamine
+        self.draw_columns(self.times3[stamp]) # Joonista graafik. Tuleb veel läbi mõelda...
+        # list(sorted(sts.times2,key=lambda x:datetime.datetime.strptime(x,sts.ajaformaat)))
+        self.draw_timestamp(stamp)        # Lisa ajamärge
+        pygame.display.flip()
+        pygame.image.save(self.lava, 'gif/screenshot_'+"{:03d}".format(self.counter)+'_'+stamp+'.jpeg')
+        self.counter+=1
+    def modifyEachValueInDictionary(self,old,new,decay_a,decay_b):
+        # max([0,decay_a*old[kanal]-decay_b,new[kanal]])
+        # max([0,decay_a*old[kanal]-decay_b])
+        out=dict()
+        if new:
+            for k,v in new.items():
+                out[k]=v
+        for k,v in old.items():
+            if k in out:
+                out[k]=max([0,decay_a*v-decay_b,out[k]])
+            else:
+                out[k]=max([0,decay_a*v-decay_b])
+        return out
+    def draw_main(self):
+        """Ma ei tea, milleks"""
+        self.lava = pygame.display.set_mode((self.width, self.hei), 0, 32)
+        self.lava.fill([0,0,0])
+        self.times3=dict()
+        decay_a=0.8
+        decay_b=1
+        # datetime.datetime(2018, 8, 11, 2, 0)+datetime.timedelta(hours=1)
+        if self.day0.hour==0==self.day9.hour and '%H' not in self.sts.ajaformaat:
+            delta = datetime.timedelta(days=1)
+        else:
+            delta = datetime.timedelta(hours=1)
+        datum=self.day0
+        self.counter=0
+        dat_prev=0
+        # Esimene kord
+        date_text=datetime.datetime.strftime(datum,self.sts.ajaformaat)
+        self.times3[date_text]=self.sts.times2[date_text]
+        dat_prev=date_text
+        self.draw(date_text)
+        datum += delta
+        while datum<=self.day9:
+            date_text=datetime.datetime.strftime(datum,self.sts.ajaformaat)          
+            if date_text in self.sts.times2:
+                new=self.sts.times2[date_text]
+            else:
+                new=[]
+            old=self.times3[dat_prev]
+            dat_prev=date_text
+            combined=dict()
+            for kanal in set(old).union(set(new)):
+                if kanal in new and kanal in old:
+                    combined[kanal]=self.modifyEachValueInDictionary(old[kanal],new[kanal],decay_a,decay_b)
+                elif kanal in new:
+                    combined[kanal]=new[kanal]
+                else:
+                    combined[kanal]=self.modifyEachValueInDictionary(old[kanal],None,decay_a,decay_b)
+
+            self.times3[date_text]=combined
+            self.draw(date_text)
+            datum += delta
+        pygame.quit()
 
 sts = Stats()
-#"""
+sts.times2_cleanup()
+"""
 for i in list(filter(lambda x: x[0] != '_', dir(sts))):
     if type(eval('sts.' + i)).__name__ == 'type':
         for x in list(filter(lambda x: x[0] != '_', dir(eval('sts.' + i)))):
@@ -494,3 +750,11 @@ for i in list(filter(lambda x: x[0] != '_', dir(sts))):
     else:
         print('sts.' + i, str(eval('sts.' + i))[:35].strip(), sep='\t')
 #"""
+import pygame
+from math import log
+
+# pygame.init()
+# ani = Animate(sts)
+# ani.draw_main()
+
+            
