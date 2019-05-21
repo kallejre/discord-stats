a143b90decb68a517640dc55f07d84e293034ce1
kallejre <kallejre@yahoo.com>
2019-05-21 23:09:20 +0300

    boti automaatkäivitus töötab

--- Bot/bot_runner.py
+++ Bot/bot_runner.py
@@ -1,8 +1,8 @@
 import os, sys, subprocess
 #os.system('python3 ./bot4.6')
-
 os.chdir(os.path.abspath("."))
-print(os.system("echo %CD%"))
-
-os.system('python bot4.7.py')
-subprocess.call(sys.executable + ' "' + os.path.realpath(__file__) + '"')
+try:
+    os.system('python3 bot4.7.py')
+except SystemExit:
+    pass
+os.system('python3 bot_runner.py')
